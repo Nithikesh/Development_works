c4c0c520b266582574d9dec96eaea96f878cd69d
Nithikesh <93579617+Nithikesh@users.noreply.github.com>
2021-11-23 23:16:07 +0530

    Add files via upload

---
+++ ITcompany.java
@@ -0,0 +1,22 @@
+public class ITcompany
+{
+static
+{
+System.out.println("Internet on");
+System.out.println("Swipe on");
+}
+	
+	{
+System.out.println("login");
+	}
+	public ITcompany()
+	{
+		System.out.println("I'm nithikesh");
+	}
+public static void main (String [] args)
+{
+	System.out.println("Main method");
+ITcompany emp1 = new ITcompany();
+ITcompany emp2 = new ITcompany();
+}
+}
